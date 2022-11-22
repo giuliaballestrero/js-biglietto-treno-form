8586170020c5729a79f4b8fe8d06f88d8188e0ca
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-22 17:31:38 +0100

    Add age variables

--- js/script.js
+++ js/script.js
@@ -10,9 +10,21 @@ button.addEventListener('click', function() {
     console.log(inputDistance.value);   
     console.log(inputAge.value);
 
-// creo le variabili in base alle fasce d'età
+//dichiaro il prezzo per km
+    let ticketPrice = (inputDistance.value * 0.2762);
+    console.log(ticketPrice);
 
-})
+// creo le variabili in base alle fasce d'età
+    if (inputAge.value < 18)
+    ticketPrice = ticketPrice - (ticketPrice * 17.5 / 100);
+
+    else if (inputAge.value > 65)
+    ticketPrice = ticketPrice - (ticketPrice * 33.5 / 100);
 
 //scrivo il prezzo finale sul documento
+    document.getElementById('output') .innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice.toFixed(2) + '&euro;';
+    console.log(ticketPrice);
+})
+
+
 
