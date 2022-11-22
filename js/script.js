6b1060187045b05e9abc596a856ad9a0c8394118
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-22 15:48:26 +0100

    Add structure

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// definisco i form per inserire i dati che devo raccogliere
+const inputDistance = document.getElementById ('inputDistance');
+const inputAge = document.getElementById ('inputAge');
+const button = document.getElementById ('button');
+
+
+// creo una funzione per raccogliere i dati dopo aver cliccato il bottone
+button.addEventListener('click', function() {
+    console.log(inputDistance.value);   
+    console.log(inputAge.value);
+
+// creo le variabili in base alle fasce d'età
+
+})
+
+//scrivo il prezzo finale sul documento
+
