cadd6e4eaf2d1becaaec4b1b8de26b59c9d049f8
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-23 10:42:17 +0100

    Add correct value outside if

--- js/script.js
+++ js/script.js
@@ -10,17 +10,22 @@ button.addEventListener('click', function() {
     console.log(inputDistance.value);   
     console.log(inputAge.value);
 
-//dichiaro il prezzo per km
-    let ticketPrice = (inputDistance.value * 0.2762);
+//dichiaro i valori
+const distance = parseInt (inputDistance.value)
+const age = parseInt (inputAge.value)
+const kmPrice = 0.2762
+
+//dichiaro la variabile del prezzo del biglietto
+    let ticketPrice = (distance * kmPrice);
     console.log(ticketPrice);
 
 // creo le variabili in base alle fasce d'età
-    if (inputAge.value < 18)
+    if (age < 18) {
     ticketPrice = ticketPrice - (ticketPrice * 17.5 / 100);
-
-    else if (inputAge.value > 65)
-    ticketPrice = ticketPrice - (ticketPrice * 33.5 / 100);
-
+    }   else if (age > 65) {
+        ticketPrice = ticketPrice - (ticketPrice * 33.5 / 100);
+    }
+    
 //scrivo il prezzo finale sul documento
     document.getElementById('output') .innerHTML = 'Il prezzo del tuo biglietto è: ' + ticketPrice.toFixed(2) + ' &euro;';
     console.log(ticketPrice);
